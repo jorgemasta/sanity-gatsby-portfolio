f5fe775e3a8a20d47955846da1015e311b5905c3
jorgemasta <jorgems93@gmail.com>
2020-06-14 22:58:12 +0100

    feat: change the code highlighter to Prism

--- web/src/components/code.js
+++ web/src/components/code.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import SyntaxHighlighter from 'react-syntax-highlighter'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {atomDark} from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+function parseLanguage (language) {
+  if (language === 'sh') return 'bash'
+  return language
+}
 
 export default ({node}) => {
   if (!node || !node.code) { return null }
   const {language, code} = node
   return (
-    <SyntaxHighlighter language={language || 'text'}>
+    <SyntaxHighlighter
+      language={parseLanguage(language) || 'text'}
+      customStyle={{marginLeft: '-1.5em', marginRight: '-1.5em'}}
+      style={atomDark}
+      showInlineLineNumbers
+    >
       {code}
     </SyntaxHighlighter>
   )
